863c86102da4f41466537c1133dcda26076d7370
Vshevchuck <vlad3032002@gmail.com>
2020-02-24 15:31:45 +0200

    Initial commit

---
+++ lesson_1/calc(1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int firstNumber=0;
+  int secondNuber=0;
+  cout<<"please enter a number"<<endl;
+  cin>>firstNumber;
+  cout<<"please enter a number"<<endl;
+  cin>>secondNuber;
+  cout<<"sum is: "<<firstNumber+secondNuber;
+}
---
+++ lesson_1/сrazySquirrel(2).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int coneN=0;
+  int nutM=0;
+  int gluttonyK=0;
+  cout<<"how many squirrel gathered nuts?"<<endl;
+  cin>>coneN;
+  cout<<"how many nuts in cone?"<<endl;
+  cin>>nutM;
+  cout<<"how much is needed for the winter?"<<endl;
+  cin>>gluttonyK;
+  int squirrelHave=coneN*nutM;
+  if(squirrelHave >= gluttonyK)
+  {
+      cout<<"Yes";
+  }
+  else
+    cout<<"no";
+}
