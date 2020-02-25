ee5ea63c7af55b323437ea1b5215f40316eeea82
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-02-25 02:03:14 +0200

    Add files via upload

---
+++ lesson_1/Task 6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a=0;
+  int b=0;
+  int c=0;
+  cout<<"enter first number"<<endl;
+  cin>>a;
+  cout<<"enter second number"<<endl;
+  cin>>b;
+  cout<<"enter third number"<<endl;
+  cin>>c;
+  if(a*b==c || a*c==b || b*c==a)
+  {
+      cout<<"yes";
+  }
+  else
+    cout<<"no";
+}
+
