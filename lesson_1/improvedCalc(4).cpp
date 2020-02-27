99c539e1b50998b763ba3bd1b1530c0ca8d9bfa5
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-02-27 17:05:56 +0200

    changed if to case in lesson 1 task 4
    fixed indintation in lesson 1 task 4

---
+++ lesson_1/improvedCalc(4).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstNumer=0;
+    int secondNumber=0;
+    int ourAct=0;
+    cout<<"insert the number"<<endl;
+    cin>>firstNumer;
+    cout<<"insert the number"<<endl;
+    cin>>secondNumber;
+    cout<<"if you want the amount, click 1"<<endl;
+    cout<<"if you want a difference, press 2"<<endl;
+    cout<<"if you want multiplication, press 3"<<endl;
+    cout<<"if you want division, press 4"<<endl;
+    cin>>ourAct;
+    if(ourAct > 4 || ourAct < 1)
+    {
+        cout<<"error";
+    }
+    switch(ourAct)
+    {
+        case 1:
+            cout<<firstNumer+secondNumber;
+            break;
+        case 2:
+            cout<<firstNumer-secondNumber;
+            break;
+        case 3:
+            cout<<firstNumer*secondNumber;
+            break;
+        case 4:
+            cout<<firstNumer*secondNumber;
+            break;
+    }
+}
