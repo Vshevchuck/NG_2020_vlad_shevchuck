6e18b8a0a4286c19be31e45984f294cd0665d1dd
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-02 22:19:07 +0200

    added lesson 2 task 1

---
+++ lesson 2/stars(1).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int quantity=0;
+    cout<<"How many stars you want?"<<endl;
+    cin>>quantity;
+    for(int i=0;i<quantity;i++)
+    {
+        cout<<"*";
+    }
+
+}
