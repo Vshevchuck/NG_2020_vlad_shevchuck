df8a7b619addbea4e8b3fc50a2bb3849f0c4aa7d
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-02 22:50:39 +0200

    added lesson 2 task 2

---
+++ lesson 2/nubers(2).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numbers=0;
+    cout<<"enter number and we show all numbers from 0 to your number"<<endl;
+    cin>>numbers;
+    for(int i=0;i<=numbers;i++)
+    {
+        cout<<i<<" ";
+    }
+
+}
