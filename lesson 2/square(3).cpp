58551efb75e50e20cb8ae301b170123cfc88d392
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-02 23:12:35 +0200

    added lesson 2 task 3

---
+++ lesson 2/square(3).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sizeSquare=0;
+    cout<<"enter size your square"<<endl;
+    cin>>sizeSquare;
+    for(int i=0;i<sizeSquare;i++)
+    {
+        for(int j=0;j<sizeSquare;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
