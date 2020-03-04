1456ed6479ed4044f677c5da33952324eae16690
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-04 23:41:11 +0200

    added Lesson 2 Task 4

---
+++ lesson 2/staircase_and_pyramid(4).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"part  a"<<endl;
+    int sizeSquare=0;
+    cout<<"enter size your ladder "<<endl;
+    cin>>sizeSquare;
+    for(int countSize=0;countSize<sizeSquare;countSize++)
+    {
+        for(int countStars=sizeSquare;countStars-countSize<=sizeSquare;countStars++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<"part  b"<<endl;
+    sizeSquare=0;
+    cout<<"enter size your ladder "<<endl;
+    cin>>sizeSquare;
+    for(int countSize=0;countSize<sizeSquare;countSize++)
+    {
+        for(int countStars=0;countStars<sizeSquare-countSize;countStars++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<"part 3 pyramid  "<<endl;
+    sizeSquare=0;
+    cout<<"enter size your ladder "<<endl;
+    cin>>sizeSquare;
+    for(int countSize=0;countSize<sizeSquare;countSize++)
+    {
+        for(int tabs=0;tabs<sizeSquare-countSize-1;tabs++)
+        {
+         cout<<" ";
+        }
+        for(int countStars=0;countStars<=countSize*2;countStars++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
