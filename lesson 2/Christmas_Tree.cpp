d875881c91f470efa18a6551bd0603aeddb9f4dc
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-05 21:05:17 +0200

    added Task 5 Lesson 2

---
+++ lesson 2/Christmas_Tree.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sizeSquare=0;
+    cout<<"enter size your christmas tree "<<endl;
+    cin>>sizeSquare;
+    for(int countSize=0;countSize<sizeSquare;countSize++)
+    {
+        for(int tabs=0;tabs<sizeSquare-countSize-1;tabs++)
+        {
+         cout<<" ";
+        }
+        for(int countStars=0;countStars<=countSize*2;countStars++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+    for(int tabs=0;tabs<=sizeSquare-2;tabs++)
+    {
+        cout<<" ";
+    }
+    cout<<"*";
+}
