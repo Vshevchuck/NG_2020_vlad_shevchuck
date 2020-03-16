1dc03ff5f365af44e6f19780b72a29371cdb8cec
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-16 17:56:31 +0200

    Add files via upload

---
+++ lesson_3/dots.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dots[5];
+    int check=0;
+    int stopPaint=0;
+    for(int counter=0;counter<5;counter++)
+    {
+        cout<<"how much dots you need?"<<endl;
+        cin>>dots[counter];
+    }
+    while(stopPaint == 0)
+    {
+
+        for(int counter=0;counter<5;counter++)
+        {
+            if(dots[counter]>0)
+            {
+                cout<<"*";
+                dots[counter]--;
+            }
+            else{
+                cout<<" ";
+            }
+
+        }
+        for(int counter=0;counter<5;counter++)
+        {
+            if(dots[counter]==0)
+            {
+               check++;
+            }
+            if(check==5)
+            {
+                stopPaint=1;
+            }
+        }
+        check=0;
+        cout<<endl;
+    }
+
+}
