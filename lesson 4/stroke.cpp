8a99f819dcaf6cc00ea9aab1cebe69439cb0a79b
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-17 23:28:00 +0200

    added Lesson 4 Task 1

---
+++ lesson 4/stroke.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char arr[100];
+    int index=0;
+    int counter=0;
+    for(int cleaner=0;cleaner<100;cleaner++)
+    {
+        arr[cleaner]=0;
+    }
+    cout<<"enter your string"<<endl;
+    cin.getline(arr,100);
+    if(arr[index] != 0)
+    {
+        while(arr[index] != 0)
+        {
+            if(arr[index] == ' ' || arr[index] ==',' || arr[index] == '.' || arr[index] == '!' || arr[index] == '!' || arr[index] == ':')
+            {
+                counter++;
+            }
+            index++;
+        }
+        cout<<"your string has "<<counter<<" words"<<endl;
+    }
+    else{
+        cout<<"your string has not words"<<endl;
+    }
+
+
+
+}
