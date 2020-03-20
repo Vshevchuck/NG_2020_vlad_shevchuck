e73d7529c4e8c7e5ac9a37b6476b931ac98c026b
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-21 00:20:13 +0200

    fixed array in Lesson 3 Task 4

--- lesson_3/dots.cpp
+++ lesson_3/dots.cpp
@@ -5,31 +5,34 @@ using namespace std;
 int main()
 {
     int dots[5];
+    int secondDots[5];
     int check=0;
     int stopPaint=0;
+
     for(int counter=0;counter<5;counter++)
     {
         cout<<"how much dots you need?"<<endl;
         cin>>dots[counter];
+    }
+    for(int counter=0;counter<5;counter++)
+    {
+       secondDots[counter]=dots[counter];
     }
     while(stopPaint == 0)
     {
 
         for(int counter=0;counter<5;counter++)
         {
-            if(dots[counter]>0)
+            if(secondDots[counter]>0)
             {
                 cout<<"*";
-                dots[counter]--;
+                secondDots[counter]--;
             }
             else{
                 cout<<" ";
             }
 
-        }
-        for(int counter=0;counter<5;counter++)
-        {
-            if(dots[counter]==0)
+            if(secondDots[counter]==0)
             {
                check++;
             }
@@ -38,8 +41,8 @@ int main()
                 stopPaint=1;
             }
         }
-        cout<<endl;
         check=0;
+        cout<<endl;
     }
 
 }
