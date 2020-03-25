5ba2d4a06ed13748b5fa451892645936b7b4e067
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-03-26 00:12:04 +0200

    added Lesson 4 Task 2

---
+++ lesson 4/big_word.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char arr[100];
+    int index=0;
+    int counter=0;
+    int stop=0;
+    int check=0;
+    int lock=0;
+    int second=0;
+    int first=0;
+    int maxString=0;
+    for(int cleaner=0;cleaner<100;cleaner++)
+    {
+        arr[cleaner]=0;
+    }
+    cout<<"enter your string"<<endl;
+    cin.getline(arr,100);
+    if(arr[index] != 0)
+    {
+
+        while(arr[index] != 0)
+        {
+
+            if((arr[index]>='a' && arr[index]<='z') || (arr[index]>='A' && arr[index]<='Z'))
+            {
+
+                if(stop==0)
+                {
+                    if(lock==0){
+                        check=index;
+                        lock=1;
+                    }
+
+                    counter++;
+                }
+
+                if((arr[index+1] <'A' || (arr[index+1] >'Z' && arr[index+1]<'a') || (arr[index+1]>'z')))
+                {
+                     stop=1;
+                     if(maxString<counter)
+                     {
+                         maxString=counter;
+                         second=index;
+                         first=check;
+                     }
+                     lock=0;
+                     counter=0;
+                }
+
+            }
+            stop=0;
+            index++;
+        }
+        index=first;
+        while(index<=second)
+        {
+            cout<<arr[index];
+            index++;
+        }
+    }
+}
