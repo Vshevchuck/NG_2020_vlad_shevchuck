f65ff347d592430a38a7baf119bad9c835c3ef1c
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-04-02 03:03:38 +0300

    fixed all in Task Lesson 4 Task 2

--- lesson 4/big_word.cpp
+++ lesson 4/big_word.cpp
@@ -4,62 +4,45 @@ using namespace std;
 
 int main()
 {
-    char arr[30];
-    int index=0;
-    int counter=0;
-    int stop=0;
-    int check=0;
-    int lock=0;
-    int second=0;
-    int first=0;
-    int maxString=0;
-    for(int cleaner=0;cleaner<30;cleaner++)
+    char arr[50];
+    int index = 0;
+    int counter = 0;
+    int maxString = 0;
+    int pointWrite=0;
+    cout << "enter your string"<<endl;
+    cin.getline(arr, 50);
+    while (arr[index] != 0)
     {
-        arr[cleaner]=0;
-    }
-    cout<<"enter your string"<<endl;
-    cin.getline(arr,30);
-    if(arr[index] != 0)
-    {
-
-        while(arr[index] != 0)
+        if (arr[index] >= 'A' && arr[index] <= 'Z'  || (arr[index]>='a' && arr[index]<='z'))
         {
-
-            if((arr[index]>='a' && arr[index]<='z') || (arr[index]>='A' && arr[index]<='Z'))
+            counter++;
+        }
+        if((arr[index] <'A' || (arr[index] >'Z' && arr[index]<'a') || (arr[index]>'z')))
+        {
+            if (counter > maxString)
             {
-
-                if(stop==0)
-                {
-                    if(lock==0){
-                        check=index;
-                        lock=1;
-                    }
-
-                    counter++;
-                }
-
-                if((arr[index+1] <'A' || (arr[index+1] >'Z' && arr[index+1]<'a') || (arr[index+1]>'z')))
-                {
-                     stop=1;
-                     if(maxString<counter)
-                     {
-                         maxString=counter;
-                         second=index;
-                         first=check;
-                     }
-                     lock=0;
-                     counter=0;
-                }
-
+                maxString = counter;
+                pointWrite = index - maxString;
             }
-            stop=0;
-            index++;
+            counter = 0;
         }
-        index=first;
-        while(index<=second)
+        if(arr[index + 1] == 0)
         {
-            cout<<arr[index];
-            index++;
+            if (counter > maxString)
+            {
+                maxString = counter;
+                pointWrite = index - maxString + 1;
+            }
         }
+        index++;
     }
+    index=0;
+    while(index < maxString)
+    {
+        cout << arr[pointWrite];
+        pointWrite++;
+        index++;
+    }
+
+
 }
