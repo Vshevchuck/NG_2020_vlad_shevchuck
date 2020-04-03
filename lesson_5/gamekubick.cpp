42545ae810613f1c734ca28f1fd51e55bd2a3ba5
Vshevchuck <59417922+Vshevchuck@users.noreply.github.com>
2020-04-04 01:58:34 +0300

    added rand and fixed some func Lesson 5 Task 2

--- lesson_5/gamekubick.cpp
+++ lesson_5/gamekubick.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
-int generalPoints(int cubsdots,int cubscounter)
-{
-    int points;
-    points=cubsdots*cubscounter;
-    return points;
+
+int getResault(int cubeSize, int cubes){
+    srand(time(NULL));
+    int points = 0;
+    for(int counter = 0; counter < cubes; counter++)
+    {
+        points += (rand()%cubeSize +1);
+    }
+  return points;
 }
+
 int main()
 {
-    int points;
-    int cubsdots;
-    int cubscounter;
-    cout<<"enter cube's dots ";
-    cin>>cubsdots;
-    cout<<"enter count cobes ";
-    cin>>cubscounter;
-    points=generalPoints(cubsdots,cubscounter);
-    cout<<"all points-"<<points;
-
+    int cubes = 0;
+    int cubeSize = 0;
+    cout << "Enter cubes count- ";
+    cin >> cubes;
+    cout << "Enter cubes size - ";
+    cin >> cubeSize;
+    cout << "Your points - " << getResault(cubeSize, cubes);
 }
